8f84f55df3863325056601674a2e6b71aff84499
songyingxinok <johnsong@johntekiMacBook-Pro.local>
2013-10-19 21:25:31 +0800

    Initial Commit

---
+++ DynamicScrollViewDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DynamicScrollViewDemo
+//
+//  Created by john song on 13-10-19.
+//  Copyright (c) 2013年 john song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
